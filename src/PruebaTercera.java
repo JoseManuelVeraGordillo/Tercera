e604d11fa03b23146e91d9b4114c7f25e7235c5e
JoseManuelVeraGordillo <95566034+JoseManuelVeraGordillo@users.noreply.github.com>
2021-12-14 09:08:07 +0100

    Prueba Tercera
    Texto de prueba

---
+++ src/PruebaTercera.java
@@ -0,0 +1,10 @@
+
+public class PruebaTercera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola Negro");
+System.out.println("Joder, Mira que te cuesta....");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Tercera {
+}
